4153798c27729c3316358a96453496d10d3d18f2
Sang-heon <choi.sg@obmontessori.edu.ph>
2025-04-08 11:14:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+    // calculate js event
+    function calculate(){
+        var weight = document.getElementById("weight").value;
+        var height = document.getElementById("height").value;
+        
+        let bmi = weight / Math.pow(height, 2);
+        let bmiround = Math.round(bmi);
+
+        // Underweight
+            if (bmiround < 18.5){
+                window.alert('Your BMI Index is '+ bmi +', meaning you are on the underweight scale.');
+            }
+            else {
+                window.location.reload();
+            }
+        
+        // Normal weight
+            if (bmiround >= 18.5 && bmiround <= 24.9){
+                window.alert('Your BMI Index is '+ bmi +', meaning you are on the normal weight scale.');
+            }
+            else {
+                window.location.reload();
+            }
+
+        // Overweight
+            if (bmiround >= 25 && bmiround <= 29.9){
+                window.alert('Your BMI Index is '+ bmi +', meaning you are on the overweight scale.');
+            }
+            else {
+                window.location.reload();
+            }
+
+        // Obese
+            if (bmiround >= 30){
+                window.alert('Your BMI Index is '+ bmi +', meaning you are on the obese scale.');
+            }
+            else {
+                window.location.reload();
+            }
+    }
+
+    // refresh js event
+    function refresh(){
+        window.location.reload();
+    }
